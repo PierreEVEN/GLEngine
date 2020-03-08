dc5ca1fbb65f630c29c5834fd7dcc6a26ddb8ff2
even <evenpierre@orange.fr>
2020-03-08 16:57:25 +0100

    azf

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,5 +1,5 @@
 @echo off
 
 echo building project files
-cd .\Tools\ProjectBuilder\bin\Debug\netcoreapp3.1\
+cd .\Tools\ProjectBuilder\bin\Release\netcoreapp3.1\
 start /wait ProjectBuilder.exe %CD%
